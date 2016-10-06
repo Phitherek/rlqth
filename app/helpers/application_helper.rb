c3656adbd179453cd0b5fa52f3b9e1f18b21e1ce
Phitherek_ <phitherek@gmail.com>
2016-10-06 02:49:52 +0200

    Fix bootstrap pagination style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def paginate objects, options = {}
-    options.reverse_merge!( theme: 'twitter-bootstrap' )
+    options.reverse_merge!( theme: 'twitter-bootstrap-3' )
 
     super( objects, options )
   end
